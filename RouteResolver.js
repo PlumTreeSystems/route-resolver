5b0f3ee17279f50b10a0091345a2a63f7d309375
aTaQ <deividas.liaudanskas@gmail.com>
2016-11-04 23:37:25 +0200

    Added ability to handle route parameters

--- RouteResolver.js
+++ RouteResolver.js
@@ -11,12 +11,33 @@ class RouteResolver {
         var configObj = config;
         this.routes = {};
         for(var i in configObj.routes){
-            this.routes[i] = configObj.routes[i];
+            this.routes[i] = {
+                value : configObj.routes[i].value,
+                defaultParameters : configObj.routes[i].defaultParameters ? configObj.routes[i].defaultParameters : {},
+            };
         }
     }
-    //Method that returns route by Name
-    get(name){
+    _insertPlaceholderValues(path,values){
+         var newPath = path.replace(/{\w+}/g,function(placeholder){
+             placeholder = placeholder.substring(1, placeholder.length-1);
+             return values[placeholder] || "";
+         });
+        return newPath;
+    }
+    _fetchRoute(name){
         return this.routes[name];
+    }
+    _resolvePath(name,params){
+        var route = this._fetchRoute(name);
+        var allParams = route.defaultParameters;
+        for(var i in params){
+            allParams[i] = params[i];
+        }
+        return this._insertPlaceholderValues(route.value, allParams);
+    }
+    //Method that returns route by Name, params is optional
+    get(name,params = {}){
+        return this._resolvePath(name, params);
     }
 }
 
