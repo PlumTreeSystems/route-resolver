1854a193a08885df8603f97865c5dd66b681f328
aTaQ <deividas.liaudanskas@gmail.com>
2016-12-03 02:41:55 +0200

    Update to work with older browsers

--- RouteResolver.js
+++ RouteResolver.js
@@ -1,46 +1,69 @@
+"use strict";
+
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
 /*
-Expected Structure of Config
-    {
-        routes:{
-            dash: "/investor/dash"
-        }
-    }
-*/
-class RouteResolver {
-    constructor(config){
+ Expected Structure of Config
+ {
+ routes:{
+ dash: "/investor/dash"
+ }
+ }
+ */
+var RouteResolver = function () {
+    function RouteResolver(config) {
+        _classCallCheck(this, RouteResolver);
+
         var configObj = config;
         this.routes = {};
-        for(var i in configObj.routes){
+        for (var i in configObj.routes) {
             this.routes[i] = {
-                value : configObj.routes[i].value,
-                defaultParameters : configObj.routes[i].defaultParameters ? configObj.routes[i].defaultParameters : {},
+                value: configObj.routes[i].value,
+                defaultParameters: configObj.routes[i].defaultParameters ? configObj.routes[i].defaultParameters : {}
             };
         }
     }
-    _insertPlaceholderValues(path,values){
-         var newPath = path.replace(/{\w+}/g,function(placeholder){
-             placeholder = placeholder.substring(1, placeholder.length-1);
-             return values[placeholder] || "";
-         });
-        return newPath;
-    }
-    _fetchRoute(name){
-        return this.routes[name];
-    }
-    _resolvePath(name,params){
-        var route = this._fetchRoute(name);
-        var allParams = route.defaultParameters;
-        for(var i in params){
-            allParams[i] = params[i];
-        }
-        return this._insertPlaceholderValues(route.value, allParams);
-    }
-    //Method that returns route by Name, params is optional
-    get(name,params = {}){
-        return this._resolvePath(name, params);
-    }
-}
 
-module.exports = function routeResolverBuilder (config){
+    _createClass(RouteResolver, [{
+        key: "_insertPlaceholderValues",
+        value: function _insertPlaceholderValues(path, values) {
+            var newPath = path.replace(/{\w+}/g, function (placeholder) {
+                placeholder = placeholder.substring(1, placeholder.length - 1);
+                return values[placeholder] || "";
+            });
+            return newPath;
+        }
+    }, {
+        key: "_fetchRoute",
+        value: function _fetchRoute(name) {
+            return this.routes[name];
+        }
+    }, {
+        key: "_resolvePath",
+        value: function _resolvePath(name, params) {
+            var route = this._fetchRoute(name);
+            var allParams = route.defaultParameters;
+            for (var i in params) {
+                allParams[i] = params[i];
+            }
+            return this._insertPlaceholderValues(route.value, allParams);
+        }
+        //Method that returns route by Name, params is optional
+
+    }, {
+        key: "get",
+        value: function get(name) {
+            var params = arguments.length <= 1 || arguments[1] === undefined ? {} : arguments[1];
+
+            return this._resolvePath(name, params);
+        }
+    }]);
+
+    return RouteResolver;
+}();
+
+module.exports = function routeResolverBuilder(config) {
     return new RouteResolver(config);
-}
+};
